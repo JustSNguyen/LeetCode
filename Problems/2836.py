daec9cd9a953918a4771dcb90558d2f021d8e4b6
nguyen <nguyensvo123@gmail.com>
2023-09-12 22:54:32 +0700

    Complete Leetcode 2836

--- Problems/2836.py
+++ Problems/2836.py
@@ -1,20 +1,32 @@
 import math
 from typing import List
+from functools import lru_cache
 
 class Solution:
     def getMaxFunctionValue(self, next_receiver: List[int], k: int) -> int:
         log_2_k = math.floor(math.log(k, 2))
         number_of_passers = len(next_receiver)
         receiver = [[0 for _ in range(log_2_k + 1)] for _ in range(number_of_passers)]
+        sum_of_receiver = [[0 for _ in range(log_2_k + 1)] for _ in range(number_of_passers)]
 
         for u in range(number_of_passers):
             receiver[u][0] = next_receiver[u]
+            sum_of_receiver[u][0] = u + next_receiver[u]
 
         for v in range(1, log_2_k + 1):
             for u in range(number_of_passers):
-                receiver[u][v] = receiver[receiver[u][v - 1]][v - 1]
+                first_receiver = receiver[u][v-1]
+                receiver[u][v] = receiver[first_receiver][v-1]
 
+                first_sum = sum_of_receiver[u][v-1] 
+                second_sum = sum_of_receiver[first_receiver][v - 1]
+                sum_of_receiver[u][v] = first_sum + second_sum - first_receiver
+        
+        power_of_two = [1 for _ in range(log_2_k + 1)]
+        for i in range(1, log_2_k + 1):
+            power_of_two[i] = 2 * power_of_two[i - 1]
 
+        @lru_cache(maxsize=3400000)
         def calculate_passing_value(current_passer, j):
             if j == 0:
                 return current_passer 
@@ -22,21 +34,23 @@ class Solution:
             if j == 1:
                 return current_passer + next_receiver[current_passer]
 
-            for next_j in range(log_2_k, -1, -1):
-                if 2 ** next_j < j:
-                    next_passer = receiver[current_passer][next_j]
-                    return calculate_passing_value(current_passer, 2 ** next_j) + calculate_passing_value(receiver[next_passer][0], j - 2 ** next_j - 1) 
-                    
+            log_2_j = math.floor(math.log(j, 2))
+            next_passer = receiver[current_passer][log_2_j]
+            return sum_of_receiver[current_passer][log_2_j] + calculate_passing_value(next_passer, j - power_of_two[log_2_j]) - next_passer
+            
         
-        return calculate_passing_value(2, 4)
+        max_result = 0 
+        for passer in range(number_of_passers):
+            max_result = max(max_result, calculate_passing_value(passer, k))
 
+        return max_result
 
         
 
 if __name__ == '__main__':
     sol = Solution()
-    receiver = [2,0,1]
-    k = 5
+    receiver = [1,1,1,2,3]
+    k = 3
     result = sol.getMaxFunctionValue(receiver, k)
     print(result)
         
