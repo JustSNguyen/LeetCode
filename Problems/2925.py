992c6359eac87a62aea01a10803ec2af6afccfbf
JustSNguyen <nguyensvo123@gmail.com>
2023-11-13 15:26:22 +0700

    improve problem 2925

--- Problems/2925.py
+++ Problems/2925.py
@@ -13,30 +13,18 @@ class Solution:
             neighbors[u].append(v)
             neighbors[v].append(u)
 
-        children = defaultdict(list)
-        processed = set()
-        stack = [0]
-        while stack:
-            cur = stack.pop()
-            if cur in processed:
-                continue
-
-            processed.add(cur)
-
-            for node in neighbors[cur]:
-                if node not in processed:
-                    children[cur].append(node)
-                    stack.append(node)
-
-        @lru_cache(maxsize=2*N)
-        def get_max_score(root, is_zero):
+        @lru_cache(maxsize=2 * N)
+        def get_max_score(root, parent, is_zero):
             if not is_zero:
                 score = values[root]
-                for child in children[root]:
-                    score += get_max_score(child, is_zero)
+                for child in neighbors[root]:
+                    if child == parent:
+                        continue
+
+                    score += get_max_score(child, root, is_zero)
                 return score
 
-            elif len(children[root]) == 0:
+            elif len(neighbors[root]) == 1 and root != 0:
                 return 0
 
             else:
@@ -46,10 +34,20 @@ class Solution:
                     if new_is_zero:
                         score = values[root]
 
-                    for child in children[root]:
-                        score += get_max_score(child, new_is_zero)
+                    for child in neighbors[root]:
+                        if child == parent:
+                            continue
+
+                        score += get_max_score(child, root, new_is_zero)
 
                     max_score = max(max_score, score)
                 return max_score
 
-        return get_max_score(0, True)
+        return get_max_score(0, -1, True)
+
+if __name__ == '__main__':
+    sol = Solution()
+    edges = [[0,1]]
+    values = [1,2]
+    result = sol.maximumScoreAfterOperations(edges, values)
+    print(result)
