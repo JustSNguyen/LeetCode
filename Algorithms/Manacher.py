9863ebebf80bb15738db580c6991e6b8921a6096
JustSNguyen <nguyensvo123@gmail.com>
2024-01-02 15:59:09 +0700

    implement manacher algorithm

--- Algorithms/Manacher.py
+++ Algorithms/Manacher.py
@@ -3,10 +3,10 @@ class Manacher:
         self.string = string
 
     def get_longest_odd_palindromes(self):
-        left = -1
-        right = -1
         N = len(self.string)
         result = [0 for _ in range(N)]
+        left = 0
+        right = 0
 
         for i in range(N):
             if i > right:
@@ -18,16 +18,17 @@ class Manacher:
                 i + result[i] + 1]:
                 result[i] += 1
 
-            if i +  result[i] > right:
+            if i + result[i] > right:
                 right = i + result[i]
                 left = i - result[i]
 
         return result
+
     def get_longest_even_palindromes(self):
-        left = -1
-        right = -1
         N = len(self.string)
         result = [0 for _ in range(N)]
+        left = 0
+        right = 0
 
         for i in range(N):
             j = i + 1
@@ -36,27 +37,18 @@ class Manacher:
             else:
                 result[i] = min(right - j + 1, result[left + right - j])
 
-            while i - result[i] >= 0 and i + result[i] + 1 < N and self.string[i - result[i]] == self.string[i +
-                                                                            result[i] + 1]:
+            while i - result[i]  >= 0 and i + result[i] + 1 < N and self.string[i - result[i]] == self.string[
+                i + result[i] + 1]:
                 result[i] += 1
 
-            if i + result[i]> right:
+            if i + result[i] > right:
                 right = i + result[i]
                 left = i - result[i] + 1
 
         return result
-class Solution:
-    def countSubstrings(self, s: str) -> int:
-        manacher = Manacher(s)
-
-        even_palindromes = sum(manacher.get_longest_even_palindromes())
-        odd_palindromes = sum(manacher.get_longest_odd_palindromes()) + len(s)
-
-        return even_palindromes + odd_palindromes
 
 
 if __name__ == '__main__':
-    s = "aaa"
-    sol = Solution()
-    result = sol.countSubstrings(s)
-    print(result)
+    string = "abbaabba"
+    manacher = Manacher(string)
+    manacher.get_longest_even_palindromes()
